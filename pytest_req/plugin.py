ba8fe9e88afb519871de3a6a33cdfd6f192c3f88
虫师 <defnngj@gmail.com>
2024-07-26 11:40:13 +0800

    feat: session add `head/patch/options`.

--- pytest_req/plugin.py
+++ pytest_req/plugin.py
@@ -153,6 +153,46 @@ class Session(requests.Session):
             url = self.base_url + url
         return self.request('DELETE', url, **kwargs)
 
+    @request
+    def patch(self, url, data=None, **kwargs):
+        r"""Sends a PATCH request. Returns :class:`Response` object.
+
+        :param url: URL for the new :class:`Request` object.
+        :param data: (optional) Dictionary, list of tuples, bytes, or file-like
+            object to send in the body of the :class:`Request`.
+        :param \*\*kwargs: Optional arguments that ``request`` takes.
+        :rtype: requests.Response
+        """
+        if (self.base_url is not None) and (url.startswith("http") is False):
+            url = self.base_url + url
+        return self.request("PATCH", url, data=data, **kwargs)
+
+    @request
+    def options(self, url, **kwargs):
+        r"""Sends a OPTIONS request. Returns :class:`Response` object.
+
+        :param url: URL for the new :class:`Request` object.
+        :param \*\*kwargs: Optional arguments that ``request`` takes.
+        :rtype: requests.Response
+        """
+        if (self.base_url is not None) and (url.startswith("http") is False):
+            url = self.base_url + url
+        kwargs.setdefault("allow_redirects", True)
+        return self.request("OPTIONS", url, **kwargs)
+
+    @request
+    def head(self, url, **kwargs):
+        r"""Sends a HEAD request. Returns :class:`Response` object.
+
+        :param url: URL for the new :class:`Request` object.
+        :param \*\*kwargs: Optional arguments that ``request`` takes.
+        :rtype: requests.Response
+        """
+        if (self.base_url is not None) and (url.startswith("http") is False):
+            url = self.base_url + url
+        kwargs.setdefault("allow_redirects", False)
+        return self.request("HEAD", url, **kwargs)
+
 
 @pytest.fixture
 def get(base_url):
--- tests/test_session.py
+++ tests/test_session.py
@@ -4,4 +4,26 @@ def test_session(session):
     """
     s = session
     s.get('https://httpbin.org/cookies/set/sessioncookie/123456789')
-    s.get('https://httpbin.org/cookies')
+    r = s.get('https://httpbin.org/cookies')
+    assert r.status_code == 200
+    # ...
+    r = s.patch('https://httpbin.org/patch')
+    assert r.status_code == 200
+    r = s.options('https://httpbin.org/get')
+    assert r.status_code == 200
+    r = s.options('https://httpbin.org/get')
+    assert r.status_code == 200
+
+
+def test_session_other(session):
+    """
+    test session
+    Sessions can also be used to provide default data to the request methods
+    """
+    s = session
+    s.post()
+    s.auth = ('user', 'pass')
+    s.headers.update({'x-test': 'true'})
+
+    # both 'x-test' and 'x-test2' are sent
+    s.get('https://httpbin.org/headers', headers={'x-test2': 'true'})
